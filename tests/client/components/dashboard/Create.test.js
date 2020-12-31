4f06fc5868e1754b5dce324fdbca783776e6202c
Christopher Docuyanan <christophejd@gmail.com>
2020-12-30 18:34:23 -0800

    Added input value test

--- tests/client/components/dashboard/Create.test.js
+++ tests/client/components/dashboard/Create.test.js
@@ -21,31 +21,39 @@ describe('<Create /> basic tests', () => {
 describe('<Create /> interactive tests', () => {
     let wrapper;
 
-    beforeAll(() => {
+    beforeEach(() => {
         wrapper = mount(<Create />);
     });
 
     it('updates component state when an input is changed', () => {
-        const textField = wrapper.find('input[name="title"]');
+        const titleField = wrapper.find('input[name="title"]');
         const descriptionField = wrapper.find('input[name="description"]');
         const genreField = wrapper.find('input[name="genre"]');
         const platformField = wrapper.find('input[name="platform"]');
         const reviewField = wrapper.find('input[name="review"]');
 
-        textField.simulate('change', { target: { name:'title', value: 'test-title' } });
+        titleField.simulate('change', { target: { name: 'title', value: 'test-title' } });
         expect(wrapper.state('title')).toEqual('test-title')
-        
-        descriptionField.simulate('change', { target: { name:'description', value: 'test-description' } });
+
+        descriptionField.simulate('change', { target: { name: 'description', value: 'test-description' } });
         expect(wrapper.state('description')).toEqual('test-description')
 
-        genreField.simulate('change', { target: { name:'genre', value: 'test-genre' } });
+        genreField.simulate('change', { target: { name: 'genre', value: 'test-genre' } });
         expect(wrapper.state('genre')).toEqual('test-genre')
 
-        platformField.simulate('change', { target: { name:'platform', value: 'test-platform' } });
+        platformField.simulate('change', { target: { name: 'platform', value: 'test-platform' } });
         expect(wrapper.state('platform')).toEqual('test-platform')
 
-        reviewField.simulate('change', { target: { name:'review', value: 'test-review' } });
+        reviewField.simulate('change', { target: { name: 'review', value: 'test-review' } });
         expect(wrapper.state('review')).toEqual('test-review')
     });
 
+    it('renders text within the respective inputs when it is changed (implies value is pulled from state)', () => {
+        const titleField = wrapper.find('input[name="title"]');
+        expect(titleField.props().value).toEqual('');
+        titleField.simulate('change', { target: { name: 'title', value: 'TEST' } });
+        expect(wrapper.state('title')).toEqual('TEST');
+        // "Re-find" the input to see the changed value
+        expect(wrapper.find('input[name="title"]').props().value).toEqual('TEST');
+    });
 })
