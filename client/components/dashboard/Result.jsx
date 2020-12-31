84bd9df1b362419bb4cb186932b62fcf1cd1d809
Giao Tran <giaotran@Giaos-MacBook-Pro.local>
2020-12-30 18:59:48 -0800

    created update.test.js, added a third test to result.test.js, modified some elements in Result.jsx, modified some text in ResultsDisplay.jsx

--- client/components/dashboard/Result.jsx
+++ client/components/dashboard/Result.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 const Result = (props) => (
   <div>
     <h2>{props.title}</h2>
+    <i><p>Description</p></i>
     <p>{props.description}</p>
+    <i><p>Genre</p></i>
     <p>{props.genre}</p>
+    <i><p>Platform</p></i>
     <p>{props.platform}</p>
+    <i><p>Review</p></i>
     <p>{props.review}</p>
+    <input type="submit" value="Update"></input>
   </div>
 );
 
